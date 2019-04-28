c478a1d59698d21fad87bfcdf0cdff7b250b8673
climbRyokawa1 <s.tempter.e2411@gmail.com>
2019-04-28 11:15:22 +0900

    GoFowardUnityChan コミット
    課題を作成したので提出します

---
+++ Assets/Lesson7 2/CubeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	 
+
+	private float speed = -0.2f;
+	private float DeadLine = -10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (this.speed, 0,0);
+		if (transform.position.x < this.DeadLine) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D Col){
+		if (Col.gameObject.tag == "CubeTag") {
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+}
